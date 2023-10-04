d0e2f6cea797e18989fd9a869ea69a37fdd5ec4b
COVAILAB1 <116708677+COVAILAB1@users.noreply.github.com>
2023-10-04 12:35:42 +0530

    Update settings.py

--- Iot_platform/Iot_platform/settings.py
+++ Iot_platform/Iot_platform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-px@tvj#f94l8e6z%^l*^1g+e438a=^cxa^66o3&pbel$_8elyv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.100.199.240']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
